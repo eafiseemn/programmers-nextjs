121cc522d50fd33aed6c1f22718c37764e361bcf
EUNJIN <eafiseemn@gmail.com>
2025-09-10 12:18:21 +0900

    [Next.js/PageRouter] ScrollTrigger / ScrollSmoother Animation 추가

--- page-router/src/layout/GlobalLayout.tsx
+++ page-router/src/layout/GlobalLayout.tsx
@@ -1,8 +1,56 @@
 import Head from 'next/head';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+import { useEffect, useRef } from 'react';
+import type { ScrollSmoother } from 'gsap/dist/ScrollSmoother';
 
 function GlobalLayout({ children }: { children: React.ReactNode }) {
+	const initialized = useRef(false);
+
+	useEffect(() => {
+		if (initialized.current) return;
+		let smoother: ScrollSmoother | undefined;
+
+		(async () => {
+			if (typeof window === 'undefined') return;
+
+			try {
+				const gsap = (await import('gsap')).default;
+				const { ScrollTrigger } = await import('gsap/dist/ScrollTrigger');
+				const { ScrollSmoother } = await import('gsap/dist/ScrollSmoother');
+
+				gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+				// DOM load가 모두 끝난 뒤에 실행
+				await new Promise((resolve) => {
+					if (document.readyState === 'complete') {
+						resolve(true);
+					} else {
+						window.addEventListener('load', () => resolve(true));
+					}
+				});
+
+				smoother = ScrollSmoother.create({
+					wrapper: '#smooth-wrapper',
+					content: '#smooth-content',
+					smooth: 1.2,
+					effects: true,
+				});
+
+				initialized.current = true;
+			} catch {
+				console.error('ScrollSmoother Initialization Failed');
+			}
+		})();
+
+		return () => {
+			if (smoother) {
+				smoother.kill();
+				initialized.current = false;
+			}
+		};
+	}, []);
+
 	return (
 		<>
 			<Head>
@@ -12,10 +60,12 @@ function GlobalLayout({ children }: { children: React.ReactNode }) {
 				<meta name='description' content='페이지 라우터를 통해 배우는 Next.js' />
 				{/* <link rel='shortcut icon' href='/vercel.svg' /> */}
 			</Head>
-			<div className='flex flex-col h-screen'>
-				<Header />
-				<main className='flex-1'>{children}</main>
-				<Footer />
+			<div id='smooth-wrapper'>
+				<div className='flex flex-col min-h-screen' id='smooth-content'>
+					<Header />
+					<main className='flex-1'>{children}</main>
+					<Footer />
+				</div>
 			</div>
 		</>
 	);
--- page-router/src/pages/index.tsx
+++ page-router/src/pages/index.tsx
@@ -1,5 +1,4 @@
-// import { useEffect } from 'react';
-
+import { useEffect, useRef } from 'react';
 import { getRandomPhotos } from '@/utils/getRandomPhotos';
 import { InferGetStaticPropsType } from 'next';
 import Head from 'next/head';
@@ -30,21 +29,60 @@ export default function Home({ data }: InferGetStaticPropsType<typeof getStaticP
 	});
   */
 
+	/* Animation */
+	// ScrollTrigger.create() 또는
+	// gsap.to({ scrollTrigger: {...} }) 형태로 사용
+
+	const listRef = useRef<HTMLUListElement>(null);
+
+	useEffect(() => {
+		let ctx: gsap.Context | undefined;
+
+		(async () => {
+			if (typeof window === 'undefined' || !listRef) return;
+
+			const gsap = (await import('gsap')).default;
+			const { ScrollTrigger } = await import('gsap/dist/ScrollTrigger');
+			gsap.registerPlugin(ScrollTrigger);
+
+			ctx = gsap.context(() => {
+				const items = gsap.utils.toArray<HTMLLIElement>('.photo-item');
+
+				items.forEach((el) => {
+					gsap.from(el, {
+						opacity: 0,
+						y: 30,
+						duration: 0.6,
+						ease: 'power2.out',
+						scrollTrigger: {
+							trigger: el,
+							start: 'top 65%', // 애니메이션 트리거 기준: (browser height의 80%로 지정된 scroller-start 지점을 element의 Top이 통과하는 순간 시작)
+							toggleActions: 'play none none reverse',
+							// markers: true, // scroll Start/End Point 표시
+						},
+					});
+				});
+			}, listRef);
+		})();
+
+		return () => ctx?.revert();
+	}, []);
+
 	return (
 		<>
 			<Head>
 				<title>Next.js Practice | Home</title>
 			</Head>
-			<div>
+			<div className='pb-5'>
 				<h2 className='text-center p-10'>
 					<strong className='text-3xl'>Triangle에서</strong>
 					<span className='block'>다양한 작가들의</span>
 					<span>사진들을 만나보세요</span>
 				</h2>
-				<ul className='flex flex-col gap-20 p-3 items-center'>
+				<ul className='flex flex-col gap-20 p-3 items-center' ref={listRef}>
 					{data.map((url, idx) => (
-						<li key={`${url}-${idx}`}>
-							<Image src={url} alt='' width={700} height={600} />
+						<li key={`${url}-${idx}`} className='photo-item'>
+							<Image src={url} alt='' width={400} height={300} />
 						</li>
 					))}
 				</ul>
