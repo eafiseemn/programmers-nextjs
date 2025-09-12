d7a5824a69a104754d27fad24e8742c464894d55
EUNJIN <eafiseemn@gmail.com>
2025-09-12 09:06:41 +0900

    [Next.js/AppRouter] Data Cache (force-cache vs no-store)

--- app-router/src/app/(top-badge-layout)/photos/page.tsx
+++ app-router/src/app/(top-badge-layout)/photos/page.tsx
@@ -1,11 +1,42 @@
+import fetchPhotos from '@/utils/fetchPhotos';
 import { Metadata } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
 
 export const metadata: Metadata = {
 	title: 'Triangle | Photos',
 	description: '다양한 작가의 다양한 작품을 감상하세요',
 };
 
-function Page() {
-	return <div>Photos Page</div>;
+async function Page() {
+	const data = await fetchPhotos(12, { cache: 'force-cache' });
+
+	if (!data) notFound(); // 404 NotFound로 Fallback
+
+	return (
+		<>
+			<h2 className='sr-only'>Photos Page</h2>
+			<ul className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4'>
+				{data.map(({ id, author, download_url, width }) => (
+					<li key={id} className='mb-4'>
+						<Link href={`/photos/${id}`}>
+							<Image
+								src={download_url}
+								alt={`${author}의 작품 - ${id}`}
+								width={500}
+								height={400}
+								style={{ width: 'auto', height: '100%' }}
+								priority={width > 4000}
+							/>
+						</Link>
+						<span className='block w-10/12 overflow-hidden text-ellipsis whitespace-nowrap'>
+							Author : {author}
+						</span>
+					</li>
+				))}
+			</ul>
+		</>
+	);
 }
 export default Page;
---
+++ app-router/src/utils/fetchPhotos.ts
@@ -0,0 +1,44 @@
+/*
+Next.js(app router)는 서버에서 fetch로 데어터를 가져올 때 Data Cache라는 저장소에 결과를 보관함
+options.cache 옵션을 통해 fetch 결과를 어떻게 캐싱/재사용할지 제어함
+
+cache: 'default'
+ - 개발환경 : 항상 새로운 요청
+ - 프로덕션 : build time에 데이터 캐싱 후 '우선' 정적 데이터 사용
+ - Dynamic API => cookies(), headers(), searchParams => 동적 데이터를 사용
+ 👉🏻 Next.js가 자동으로 SSR / SSG를 판단해서 build
+
+cache: 'force-cache'
+ - 같은 URL 요청이 있으면 Data Cache에서 꺼내 사용, 데이터가 없으면 새로 패치해서 캐시에 저장, 이후 정적 데이터 사용
+ - 강제로 캐시에 저장해서 사용하는 방식
+ - 또는 Component에서 export const dynamic = 'force-static' 으로 지정하는 방식도 가능
+ 👉🏻 build 시 한 번만 fetch -> 이후 요청은 캐시된 데이터를 그대로 사용
+ 👉🏻 데이터가 바뀌거나 수정되어도 데이터를 가져오지 않음 (revalidate 하기 전까지)
+
+cache: 'no-store'
+- 데이터를 캐시하지 않고 매번 사이트에 들어갈 때마다(on-demand) 원본 서버에서 데이터를 새롭게 요청 (Dynamic SSR)
+ - getServerSideProps 와 유사 / 실시간 데이터
+ - export const dynamic = 'force-dynamic'
+ 👉🏻 항상 새로운 요청과 새로운 데이터 (데이터의 실시간성 보장)
+ 👉🏻 데이터 요청이 많을 경우 성능 저하
+*/
+
+import { Photo } from '@/@types/type';
+
+export default async function fetchPhotos(
+	limit: number = 10,
+	init?: RequestInit
+): Promise<Photo[]> {
+	const END_POINT = `https://picsum.photos/v2/list?page=5&limit=${limit}`;
+
+	try {
+		const res = await fetch(END_POINT, { ...init });
+		if (!res.ok) {
+			throw new Error();
+		}
+		return await res.json();
+	} catch {
+		console.error('error!');
+		return [];
+	}
+}
