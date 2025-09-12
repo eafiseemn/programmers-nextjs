36bd4dadec32465c21a9183ad5e904eac9e16529
EUNJIN <eafiseemn@gmail.com>
2025-09-12 14:48:38 +0900

    [Next.js/AppRouter] Access/Refresh Token Cookie Setting

--- app-router/src/actions/createUser.action.ts
+++ app-router/src/actions/createUser.action.ts
@@ -1,5 +1,7 @@
 'use server';
 
+import { cookies } from 'next/headers';
+
 // import { revalidatePath, revalidateTag } from 'next/cache';
 // import { redirect } from 'next/navigation';
 
@@ -22,6 +24,29 @@ export default async function createUser(_: unknown, formData: FormData) {
 	if (!res.ok) {
 		throw new Error('회원가입 실패');
 	}
+
+	// BE에서 token을 JSON으로 돌려준다고 가정
+	const { accessToken, refreshToken } = await res.json();
+	const cookieStore = await cookies(); // 쿠기 굽기
+
+	// cookie setting은 BE 요청에 맞춰 진행
+	// access_token : 짧은 수명 주기
+	cookieStore.set('access_token', accessToken, {
+		httpOnly: true,
+		secure: true,
+		sameSite: 'lax', // 같은 사이트 = strict, 다른 도메인 = none, ...
+		path: '/',
+		maxAge: 60 * 15, // 15 min
+	});
+
+	// refresh_token : 긴 수명 주기
+	cookieStore.set('refresh_token', refreshToken, {
+		httpOnly: true,
+		secure: true,
+		sameSite: 'lax',
+		path: '/',
+		maxAge: 60 * 60 * 24 * 30, // 30 days
+	});
 
 	// PURGE (cleansing...)
 	// 강제 revalidate (CRUD 이후 페이지 데이터 상태 업데이트를 위해)
